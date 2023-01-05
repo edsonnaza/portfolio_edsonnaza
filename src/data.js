b86f70d1217e2e9ebc064d6138578b0bc9c098be
edsonnaza <edsonnaza@gmail.com>
2023-01-05 10:12:34 -0300

    Testimonials

--- src/data.js
+++ src/data.js
@@ -44,25 +44,25 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "It's so gratefull and amazing work with him. A men who can help you anytime when you need it, does'nt matter the time or the day.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Joel Epping - Software Engenier at rappi company.",
-    company: "https://www.linkedin.com/company/rappi/",
+      "It's so greatfull and amazing work with him. A men who can help you anytime when you need it, does'nt matter the time or the day.",
+    image: "https://randomuser.me/api/portraits/men/95.jpg",
+    name: "Joel Epping - Front End Developer at Rappi.",
+    company: "Rappi Inc.",
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
+      "I just wanted to take a moment to thank you, Edson, for making our team so special. Everything was great, the pre-planning and selection with Nanci, Kevin and his team pulled it all together and were amazing!!!!  Our entire group had a wonderful time, for a great cause.",
+    image: "https://randomuser.me/api/portraits/men/6.jpg",
+    name: "Shahriar Arif - Front End Developer at Microverse.",
+    company: "Microverse.com",
   },
 ];
 
 export const skills = [
+  "PHP/Laravel",
   "JavaScript",
   "React",
-  "Next.js",
+  "Vue.js",
   "Node",
-  "GraphQL",
   "Material UI",
 ];
