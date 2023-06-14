aa1c802533835b7b1bed359d31ba35571bf1b4b3
edsonnaza <edsonnaza@gmail.com>
2023-06-13 23:31:03 -0400

    Added photo to NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
 import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
 
+
 export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
-      
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <img
+        className="h-8 w-auto rounded-full"
+        src="./perfil-edson.jpeg"
+        alt="Eng. Edson Sanchez"
+      />
         <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
             Edson Sanchez
